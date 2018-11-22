5e75e5e2ef3a45879b69df59675d5481c2b0bdfe
JOO200 <github@joo200.de>
2018-11-22 17:30:07 +0100

    Debug-Ordner per .gitignore raus

--- MixHit/Debug/Adafruit-GFX-Library-master/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Johannes\AppData\Local\Temp\VMBuilds\MixHit\espressif_esp32\Debug\Adafruit-GFX-Library-master\glcdfont.c.o: \
- C:\Users\Johannes\Documents\Arduino\libraries\Adafruit-GFX-Library-master\glcdfont.c
